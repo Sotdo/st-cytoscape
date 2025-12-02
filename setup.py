e093c25c30bb546ffd1d9e8a8ee283ffed4a641a
Yusheng Yang <yangyusheng12323@outlook.com>
2025-12-02 13:47:38 +0800

    chore(setup): update python and streamlit requirements

--- setup.py
+++ setup.py
@@ -12,11 +12,11 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     include_package_data=True,
     classifiers=[],
-    python_requires=">=3.6",
+    python_requires=">=3.9",
     install_requires=[
         # By definition, a Custom Component depends on Streamlit.
         # If your component has other Python dependencies, list
         # them here.
-        "streamlit >= 0.63",
+        "streamlit >= 1.20",
     ],
 )
